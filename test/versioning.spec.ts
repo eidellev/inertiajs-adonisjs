edc9f64a2626b3b6a2e17ac06b0ac43a0866c1ce
eidellev <levp22@gmail.com>
2021-03-23 19:50:45 +0200

    test: added asset versioning test suite

--- test/versioning.spec.ts
+++ test/versioning.spec.ts
@@ -1,5 +1,7 @@
 import test from 'japa';
 import supertest from 'supertest';
+import { Inertia } from '../src/Inertia';
+import { HEADERS } from '../src/utils';
 import { createServer } from 'http';
 import { setup, teardown } from './utils';
 
@@ -8,5 +10,51 @@ test.group('Asset versioning', (group) => {
     await teardown();
   });
 
-  test('should ');
+  test('Should return 409 CONFLICT', async () => {
+    const props = {
+      some: {
+        props: {
+          for: ['your', 'page'],
+        },
+      },
+    };
+    const app = await setup();
+    const server = createServer(async (req, res) => {
+      const ctx = app.container.use('Adonis/Core/HttpContext').create('/', {}, req, res);
+      Inertia.version('new');
+
+      await ctx.inertia.render('Some/Page', props);
+      res.end();
+    });
+
+    await supertest(server)
+      .get('/')
+      .set(HEADERS.INERTIA_HEADER, 'true')
+      .set(HEADERS.INERTIA_VERSION, 'old')
+      .expect(409);
+  });
+
+  test('Should lazily evaluate version', async () => {
+    const props = {
+      some: {
+        props: {
+          for: ['your', 'page'],
+        },
+      },
+    };
+    const app = await setup();
+    const server = createServer(async (req, res) => {
+      const ctx = app.container.use('Adonis/Core/HttpContext').create('/', {}, req, res);
+      Inertia.version(() => 'new');
+
+      await ctx.inertia.render('Some/Page', props);
+      res.end();
+    });
+
+    await supertest(server)
+      .get('/')
+      .set(HEADERS.INERTIA_HEADER, 'true')
+      .set(HEADERS.INERTIA_VERSION, 'old')
+      .expect(409);
+  });
 });
