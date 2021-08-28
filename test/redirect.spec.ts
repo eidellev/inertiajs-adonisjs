87c8cbd58af3342c586eb417ee704472a05872c6
eidellev <levp22@gmail.com>
2021-08-28 10:41:14 +0300

    test(redirect): added tests

---
+++ test/redirect.spec.ts
@@ -0,0 +1,43 @@
+import test from 'japa';
+import supertest from 'supertest';
+import { createServer } from 'http';
+import { HEADERS } from '../src/utils';
+import { setup, teardown } from './utils';
+
+test('Redirect', (group) => {
+  test('Should set HTTP status code to 303 on PUT', async (assert) => {
+    const app = await setup();
+    const server = createServer(async (req, res) => {
+      const ctx = app.container.use('Adonis/Core/HttpContext').create('/', {}, req, res);
+      ctx.response.redirect('/some/other/route');
+      assert.equal(ctx.response.getStatus(), 303);
+      res.end();
+    });
+
+    await supertest(server).put('/').set(HEADERS.INERTIA_HEADER, 'true');
+  });
+
+  test('Should set HTTP status code to 303 on PATCH', async (assert) => {
+    const app = await setup();
+    const server = createServer(async (req, res) => {
+      const ctx = app.container.use('Adonis/Core/HttpContext').create('/', {}, req, res);
+      ctx.response.redirect('/some/other/route');
+      assert.equal(ctx.response.getStatus(), 303);
+      res.end();
+    });
+
+    await supertest(server).patch('/').set(HEADERS.INERTIA_HEADER, 'true');
+  });
+
+  test('Should set HTTP status code to 303 on DELETE', async (assert) => {
+    const app = await setup();
+    const server = createServer(async (req, res) => {
+      const ctx = app.container.use('Adonis/Core/HttpContext').create('/', {}, req, res);
+      ctx.response.redirect('/some/other/route');
+      assert.equal(ctx.response.getStatus(), 303);
+      res.end();
+    });
+
+    await supertest(server).delete('/').set(HEADERS.INERTIA_HEADER, 'true');
+  });
+});
