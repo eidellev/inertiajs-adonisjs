c0a845cbcabfb7ef81ccf1bd1d64b3e114b449dd
eidellev <levp22@gmail.com>
2021-11-20 21:30:28 +0200

    test: added middleware tests

---
+++ test/inertia-middleware.spec.ts
@@ -0,0 +1,21 @@
+import test from 'japa';
+import InertiaMiddleware from '../middleware/Inertia';
+import { setup, fs } from './utils';
+
+test.group('Inertia middleware', (group) => {
+  group.afterEach(async () => {
+    await fs.cleanup();
+  });
+
+  test('register inertia middleware', async (assert) => {
+    const app = await setup();
+
+    assert.deepEqual(app.container.use('EidelLev/Inertia/Middleware'), InertiaMiddleware);
+  });
+
+  test('Make inertia middleware instance via container', async (assert) => {
+    const app = await setup();
+
+    assert.instanceOf(app.container.make(app.container.use('EidelLev/Inertia/Middleware')), InertiaMiddleware);
+  });
+});
