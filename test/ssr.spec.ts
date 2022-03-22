b87a8c380be1128d610a5ba4cb3c0743f54544f0
eidellev <levp22@gmail.com>
2022-03-22 17:56:07 +0200

    chore(tests): added tests for react ssr

---
+++ test/ssr.spec.ts
@@ -0,0 +1,48 @@
+import test from 'japa';
+import supertest from 'supertest';
+import { codeBlock } from 'common-tags';
+import { createServer } from 'http';
+import { Inertia } from '../src/Inertia';
+import { HEADERS } from '../src/utils';
+import { setupSSR, teardown } from './utils';
+
+test.group('SSR', (group) => {
+  group.afterEach(async () => {
+    await teardown();
+    Inertia.share({});
+  });
+
+  test('Should return pre-rendered react component HTML', async (assert) => {
+    const props = {
+      some: {
+        props: {
+          for: ['your', 'page'],
+        },
+      },
+    };
+    const app = await setupSSR();
+    const server = createServer(async (req, res) => {
+      const ctx = app.container.use('Adonis/Core/HttpContext').create('/', {}, req, res);
+      const respose = await ctx.inertia.render('SomePage', props);
+
+      res.write(respose);
+      res.end();
+    });
+
+    const response = await supertest(server).get('/').expect(200);
+
+    assert.equal(
+      response.text,
+      codeBlock`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Journeyman</title>
+    </head>
+    <body><div id="app" data-page="{&quot;component&quot;:&quot;SomePage&quot;,&quot;props&quot;:{&quot;some&quot;:{&quot;props&quot;:{&quot;for&quot;:[&quot;your&quot;,&quot;page&quot;]}}},&quot;url&quot;:&quot;/&quot;}" data-reactroot=""><h1>Hello react!</h1></div>
+    </body>
+    </html>`,
+    );
+  });
+});
--- test/utils.ts
+++ test/utils.ts
@@ -75,6 +75,93 @@ export async function setup() {
   return app;
 }
 
+export async function setupSSR() {
+  await fs.add('.env', '');
+
+  await fs.add(
+    'config/app.ts',
+    codeBlock`export const appKey = '${Math.random().toFixed(36).substring(2, 38)}',
+    export const http = {
+      cookie: {},
+      trustProxy: () => true,
+    }`,
+  );
+
+  await fs.add(
+    'config/inertia.ts',
+    codeBlock`import { InertiaConfig } from '@ioc:EidelLev/Inertia';
+
+    export const inertia: InertiaConfig = {
+      view: 'app',
+      ssr: {
+        enabled: true,
+        mode: 'react'
+      }
+    };`,
+  );
+
+  await fs.add(
+    'config/session.ts',
+    codeBlock`import Env from '@ioc:Adonis/Core/Env';
+    import { SessionConfig } from '@ioc:Adonis/Addons/Session';
+
+    const sessionConfig: SessionConfig = {
+      driver: 'cookie',
+      cookieName: 'test',
+      age: '2h',
+      cookie: {
+        path: '/',
+        httpOnly: true,
+        sameSite: false,
+      },
+    }
+
+    export default sessionConfig;`,
+  );
+
+  await fs.add(
+    'resources/views/app.edge',
+    codeBlock`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Journeyman</title>
+    </head>
+    <body>
+      @if(test)
+        {{ test }}
+      @endif
+      @inertia()
+    </body>
+    </html>
+  `,
+  );
+
+  await fs.add(
+    'resources/js/Pages/SomePage.tsx',
+    codeBlock`
+    import React from 'react';
+
+    function SomePage() {
+      return React.createElement('h1', {}, 'Hello react!');
+    }
+
+    export default SomePage;
+    `,
+  );
+
+  const app = new Application(fs.basePath, 'web', {
+    providers: ['@adonisjs/core', '@adonisjs/view', '@adonisjs/session', '../../providers/InertiaProvider'],
+  });
+
+  await app.setup();
+  await app.registerProviders();
+  await app.bootProviders();
+
+  return app;
+}
+
 export async function teardown() {
   await fs.cleanup();
 }
