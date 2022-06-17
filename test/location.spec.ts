0bfb6ce5c67e45bd6753191c023eedc816473046
eidellev <levp22@gmail.com>
2022-06-17 16:59:24 +0300

    test(location): added a basic test

---
+++ test/location.spec.ts
@@ -0,0 +1,23 @@
+import { createServer } from 'http';
+import test from 'japa';
+import supertest from 'supertest';
+import { HEADERS } from '../src/utils';
+import { setup, teardown } from './utils';
+
+test.group('Location', (group) => {
+  group.afterEach(async () => {
+    await teardown();
+  });
+
+  test('Should set HTTP status code to 409 external redirect', async (assert) => {
+    const app = await setup();
+    const server = createServer(async (req, res) => {
+      const ctx = app.container.use('Adonis/Core/HttpContext').create('/', {}, req, res);
+      ctx.inertia.location('https://adonisjs.com');
+
+      res.end();
+    });
+
+    await supertest(server).put('/').set(HEADERS.INERTIA_HEADER, 'true').expect(409);
+  });
+});
