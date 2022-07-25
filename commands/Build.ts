1dff55103948057e0701f6dbbdc2f44720d74565
eidellev <levp22@gmail.com>
2022-07-25 20:40:04 +0300

    feat(commands): added `ssr:watch` and `ssr:build` commands

---
+++ commands/Build.ts
@@ -0,0 +1,30 @@
+import { existsSync } from 'fs';
+import { BaseSsrCommand } from './Base';
+
+/**
+ * Command to watch assets
+ */
+export default class Watch extends BaseSsrCommand {
+  public static commandName = 'ssr:build';
+  public static description = 'Build and watch files for changes';
+  public static settings = {
+    stayAlive: true,
+  };
+
+  public async run() {
+    const mixConfigPath = this.application.makePath(this.webpackConfig);
+
+    if (!existsSync(mixConfigPath)) {
+      this.logger.error(`Webpack configuration file '${this.webpackConfig}' could not be found`);
+      return;
+    }
+
+    const script: string = 'npx encore dev -c webpack.ssr.config.js';
+
+    const scriptEnv = {
+      NODE_ENV: 'production',
+    };
+
+    this.runScript(script, scriptEnv);
+  }
+}
---
+++ commands/index.ts
@@ -0,0 +1,1 @@
+export default ['@eidellev/inertia-adonisjs/build/commands/Build', '@eidellev/inertia-adonisjs/build/commands/Watch'];
